d1fd0580bb7d81641d8c8ce1694aa16284cb5abf
Graeme Wolfendale <gwolfendale@gmail.com>
2015-01-22 06:06:58 +0000

    Update all.js

--- all.js
+++ all.js
@@ -70,7 +70,7 @@ function zoom(d) {
   //check if the node clicked has children (i.e. is it a leaf node?)
   if(!d.children) {
     //load an appropriate wiki article for the detailed leaf display on this node.
-    $('#article').wikiblurb(
+    /*$('#article').wikiblurb(
 	  {
   		wikiURL: "http://en.wikipedia.org/",
   		apiPath: 'w',
@@ -79,7 +79,7 @@ function zoom(d) {
   		removeLinks: false,
   		type: 'all',
   		customSelector: ''
-	  });
+	  });*/
     //make the wiki article appear only once the transition animation is finished
 	   setTimeout(function(){$("div").show()}, 750);
     }
@@ -93,21 +93,4 @@ function zoom(d) {
 });
 //set the height based on diameter
 d3.select(self.frameElement).style("height", diameter + "px");
-//load a default wiki article
-$(document).ready(function()
-{
-	$('#article').wikiblurb(
-	{
-		wikiURL: "http://en.wikipedia.org/",
-		apiPath: 'w',
-		section: 0,
-		page: 'Free software',
-		removeLinks: false,
-		type: 'all',
-		customSelector: ''
-	});
-});
-//set up fancy scroll bars on wiki article
-$(document).ready(function(){
-  $('#article').tinyscrollbar();
-});
+
