d294124aeb36fa874bcaa597d86527545ebd182e
Graeme Wolfendale <gwolfendale@gmail.com>
2015-01-22 08:03:53 +0000

    Update get_course_names.py

--- get_course_names.py
+++ get_course_names.py
@@ -11,9 +11,9 @@ print("Starting scrape at:" + str(start))
 #code to pull down a list of course names
 total_scrape_time = 0
 courses_json = '''{"name": "Bolton University Courses","size": 20,"children": ['''
-for num in range(1000, 3000):
+for num in range(1, 1000):
 	course_start = timeit.default_timer()
-	url = 'http://courses.bolton.ac.uk/Details/Index/' + str(num)
+	url = 'http://courses.bolton.ac.uk/Details/Index/' + str(num+1000)
 	#get the html page at url
         page = requests.get(url)
         #parse the html into a tree
@@ -29,12 +29,8 @@ for num in range(1000, 3000):
         scrape_time = timeit.default_timer() - course_start
         total_scrape_time += scrape_time
 	print("Course scrape took " + str(timeit.default_timer() - course_start)
-	numb_courses = num - 1000
-	if numb_courses > 0:
-		average_scrape_time = total_scrape_time / numb_courses
-	else if numb_courses == 0:
-		average_scrape_time = total_scrape_time
-	remaining_courses = 3000 - num
+	average_scrape_time = total_scrape_time / num
+	remaining_courses = 1000 - num
 	print(remaining_courses + " remaining to scrape, guestimated time remaining " + (remaining_courses * average_scape_time) + " seconds")
 #remove the last comma
 courses_json = courses_json[:-1]
