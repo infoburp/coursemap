29aca80107c926a96b77fd98e4db5265f7df7fba
Graeme Wolfendale <gwolfendale@gmail.com>
2015-01-22 14:46:26 +0000

    Create get_courses_by_subject.py

---
+++ get_courses_by_subject.py
@@ -0,0 +1,25 @@
+import json
+import lxml
+from lxml import html
+import requests
+import re
+
+json_string+='''{"name": "Subjects","size":20,"children":['''
+#loop for subjects
+subjects = [68,42,46,40,79,43,50,80,45,59,44,53,54,66,49,58,57,47,77,48,67,39,75,76]
+subject_names = ["Foundation","Art","Biology","Business and Law","Chemistry","Civil Engineering","Early Years","Media","Design Technology","Distance Learning","Engineering","Computing","Health","Materials Research","Mathematics","Off Campus Overseas ","Off Campus UK","Psychology","Renewable Energy","Sport","Education","English","Top-up","University-wide"]
+for subject in subjects:
+	json_string+='''{"name":''' + subject_names[subject] + ''',"size":20,"children":['''
+	#loop for levels
+	json_string+='''{"name": "Study levels","size":20,"children":['''
+	levels = [15,16,17,19,18,20]
+	level_names = ["Undergraduate","Postgraduate","Further Education","Professional Development (Undergraduate Level)","Professional Development (Postgraduate Level)","Professional Development (Further Education Level)"]
+	for level in levels:
+		json_string+='''{"name": level_names[level],"size":20,"children":['''
+		#loop for attendance types
+		attendances=[13,14,15]
+		for attendance in attendances:
+			payload = {'SubjectAreaID': subject, 'StudyLevelID': level, 'AttendenceID': attendance}
+			r = requests.post("http://courses.bolton.ac.uk/", data=payload)
+			print(r.text)
+json_string+="]}]}]}]}"
